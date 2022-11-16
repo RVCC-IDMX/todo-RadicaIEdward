95933740689066a43f48e831885b510d01d56df4
RadicaIEdward <edward.o.galindez@gmail.com>
2022-11-16 18:14:57 -0500

    Create createList function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const listsContainer = document.querySelector('[data-lists]');
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]');
 
 let lists = [
   {
@@ -10,6 +12,26 @@ let lists = [
     name: 'todo',
   },
 ];
+
+newListForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const listName = newListInput.value;
+  if (listName == null || listName === '') {
+    return;
+  }
+  const list = createList(listName);
+  newListInput.value = null;
+  lists.push(list);
+  render();
+});
+
+function createList(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [],
+  };
+}
 
 function render() {
   clearElement(listsContainer);
