bb746856b42045f470afc48c4e535a71df5a0447
RadicaIEdward <edward.o.galindez@gmail.com>
2022-11-16 19:40:10 -0500

    Create creatTask function and newTaskForm event listener

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
 const tasksContainer = document.querySelector('[data-tasks]');
 const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput = document.querySelector('[data-new-task-input]');
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
@@ -38,8 +40,23 @@ newListForm.addEventListener('submit', (e) => {
   saveAndRender();
 });
 
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === '') return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
 function createList(name) {
   return { id: Date.now().toString(), name: name, tasks: [] };
+}
+
+function createTask(name) {
+  return { id: Date.now().toString(), name: name, complete: false };
 }
 
 function saveAndRender() {
